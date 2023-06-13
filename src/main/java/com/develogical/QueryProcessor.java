3aa092f149dfc95e9c7395f00e79791be42473d4
jackhb <jackhb@users.noreply.github.com>
2023-06-13 13:38:29 +0000

    name

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
           + "writer in the English language and the world's pre-eminent dramatist.";
     }
     if (query.toLowerCase().contains("your name")) {
-      return "RobTest";
+      return "JHB";
     }
     return "";
   }
